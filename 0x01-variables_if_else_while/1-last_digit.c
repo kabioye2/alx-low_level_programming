8b7d37309ccb83c087d49e95676d2afc2279e94d
Kehinde <kabioye2@gmail.com>
2023-05-19 19:00:09 -0700

    fixed second task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ int main(void)
         
 	if (lastDigit > 5)
         {
-                printf("and is greater than 5\n");
+                printf("%d and is greater than 5\n", lastDigit);
         }
         else if (lastDigit == 0)
         {
-                printf("and is 0\n");
+                printf("%d and is 0\n, lastDigit");
         }
         else if (lastDigit < 6)
         {
-                printf("and is less than 6 and not 0\n");
+                printf("%d and is less than 6 and not 0\n", lastDigit);
         }
         return (0);
 }
