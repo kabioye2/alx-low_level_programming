ef4d4b22343acd3c5cd428ad5a7de28dbdf0ef6e
Kehinde <kabioye2@gmail.com>
2023-05-20 18:34:29 -0700

    completed 6th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+
+	printf("\n");
+
+	return (0);
+}
