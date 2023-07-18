88dc5dad941e7cce56f7c860a90c4e967520b827
kabioye2 <kabioye2@gmail.com>
2023-07-17 22:33:02 -0700

    fix main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,32 +1,12 @@
-#include <unistd.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
-}
-
-
-/**
- * print_alphabet - Prints the alphabet in lowercase
- *
- * Return: nothing
- */
-void print_alphabet(void)
-{
-        char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-
-	putchar('\n');
+    print_alphabet();
+    return (0);
 }
