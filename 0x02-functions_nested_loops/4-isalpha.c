592dc73f0e4246c93dab22ef3fb182f6de4026b0
Kehinde Abioye <kabioye2@gmail.com>
2023-09-01 16:49:31 -0700

    main function and isalpha added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _islower - Checks for a character
+ *
+ * @c: The character to check
+ *
+ * Return: 1 for @c, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
