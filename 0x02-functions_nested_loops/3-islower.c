97a862a39477cd5de0ea9e89f06ef0420ca0a5a7
Kehinde Abioye <kabioye2@gmail.com>
2023-09-01 16:33:35 -0700

    betty style doc error corrected for islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,7 @@
 
 /**
  * _islower - function that checks for lowercase character
- * Returns 1 if c is lowercase
- * Returns 0 otherwise
+ * Returns: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
