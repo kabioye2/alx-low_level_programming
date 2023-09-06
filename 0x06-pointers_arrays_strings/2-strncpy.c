eaeb225997f3d988a59f3aeca61dc06de07f1fbd
Kehinde Abioye <kabioye2@gmail.com>
2023-09-06 08:53:24 -0700

    add strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: bytes of second string
+ * Return: a pointer to the resulting string @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+
+	return dest;
+}
