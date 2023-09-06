99dcbb4140bd88a63217292afa5eac99efbfaa4a
Kehinde Abioye <kabioye2@gmail.com>
2023-09-06 15:26:05 -0700

    add task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
