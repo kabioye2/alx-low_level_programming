ad5f3f56d8c3d4a773d7f9979d9c162403311534
Kehinde Abioye <kabioye2@gmail.com>
2023-09-06 12:26:19 -0700

    fix strncpy issues

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
 	}
 
 	return (dest);
