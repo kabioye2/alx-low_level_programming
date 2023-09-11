6896c1202ed43c37534cb30c3b7790a4cc68f4dc
Kehinde Abioye <kabioye2@gmail.com>
2023-09-11 16:00:52 -0700

    added task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the power of a number
+ * @x: base number
+ * @y: power number
+ * Return: x raised to the power y, otherwise -1 is @y is lower than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x *_pow_recursion(x, y-1));
+}
