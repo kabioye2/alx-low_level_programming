e84578a1e304aa27fff76e14690e87e31c83ceac
Kehinde Abioye <kabioye2@gmail.com>
2023-09-11 20:37:04 -0700

    added task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates natural square root of a number
+ * @n: number to be squared
+ * Return: natural square root of @n, otherwise -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int approx;
+	int next_approx;
+
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	approx = n / 2;
+	next_approx = (approx + n / approx) / 2;
+
+	if (next_approx == approx) 
+	{
+		return approx;
+	}
+
+	return _sqrt_recursive(next_approx); 
+}
