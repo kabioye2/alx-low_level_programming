67b47577013a2850871a5ecd73e8dbeb18395830
Kehinde Abioye <kabioye2@gmail.com>
2023-09-12 15:35:34 -0700

    added task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
+/**
+ * divisor - helper function
+ * @n: prime number
+ * @d: divisor
+ * Return: 1 if prime number, otherwise 0
+ */
 
+int divisor(int n, int d)
+{
+	if (d == 1)
+	{
+		return (0);
+	}
+
+	if (n % divisor == 0)
+	{
+		return (1);
+	}
+
+	return divisor(n, d - 1);
+}
+
+/**
+ * is_prime_number - find out if number is prime
+ * @n: prime number
+ * Return: 1 if prime number, otherwise 0
+ */
 
 int is_prime_number(int n)
 {
@@ -14,10 +40,5 @@ int is_prime_number(int n)
 		return (1);
 	}
 
-	if (n % (n - 1) == 0)
-	{
-		return (0);
-	}
-
-	return (is_prime_number(n - 1));
+	return (divisor(n, n - 1));
 }
