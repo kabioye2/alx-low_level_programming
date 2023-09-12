2222871a1b8d4a3a0d4a7ec75b5bc388c3b898b9
Kehinde Abioye <kabioye2@gmail.com>
2023-09-12 14:38:12 -0700

    put helper func in same file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _sqrt_recursion - calculates natural square root of a number
  * @n: number to be squared
  * Return: natural square root of @n, otherwise -1
  */
+
+int _sqrt_recursion_srch(int n, int low, int high)
+{
+	int mid;
+	int square;
+
+	if (low > high)
+	{
+		return (-1);
+	}
+
+	mid = low + (high - low) / 2;
+	square = mid * mid;
+
+	if (square == n)
+	{
+		return (mid);
+	}
+	else if (square < n)
+	{
+		return (_sqrt_recursion_srch(n, mid + 1, high));
+	}
+	else
+	{
+		return (_sqrt_recursion_srch(n, low, mid - 1));
+	}
+}
 
 int _sqrt_recursion(int n)
 {
