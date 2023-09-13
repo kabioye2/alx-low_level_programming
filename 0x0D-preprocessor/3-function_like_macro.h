98d11fc5b8aef3ca88b5665e68266e133568a7af
Kehinde Abioye <kabioye2@gmail.com>
2023-09-12 22:04:33 -0700

    fix betty style issues

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,9 @@
 #include <stdio.h>
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number for which to compute the absolute value.
+ * Return: The absolute value of x.
+ */
+
 #define ABS(x) ((x) < 0 ? -(x) : (x))
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,2 +1,10 @@
 #include <stdio.h>
+
+/**
+ * SUM - Computes the sum of two numbers.
+ * @x: The first number.
+ * @y: The second number.
+ * Return: The sum of x and y.
+ */
+
 #define SUM(x, y) ((x) + (y))
