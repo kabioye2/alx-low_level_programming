9ff0edf177163182bd09af4f5921d1fe430aceeb
Kehinde Abioye <kabioye2@gmail.com>
2023-09-13 16:24:52 -0700

    added stddef library

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - prints a name using printing function
  * @name: the name to print
  * @f: a pointer to a function
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
