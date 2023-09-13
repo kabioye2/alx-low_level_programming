88ecffdc3f18d1f407440ba2fd2e3d83003bdb22
Kehinde Abioye <kabioye2@gmail.com>
2023-09-12 19:29:10 -0700

    fix output issue

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - prints the last digit of a number
@@ -10,7 +11,7 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10; 
+	last = abs(n) % 10;
 	_putchar('0' + last);
 	return (last);
 }
