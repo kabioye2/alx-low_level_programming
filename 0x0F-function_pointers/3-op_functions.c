c7a5a3dad40caaca46e9d3c54671f557d170c47f
Kehinde Abioye <kabioye2@gmail.com>
2023-09-16 10:16:42 -0700

    fix betty style issues

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,13 @@
 #include "3-calc.h"
 
 /**
+ * This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
  * op_add - calculates sum of 2 integers
  * @a: first integer
  * @b: second integer
