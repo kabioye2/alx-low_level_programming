35da00c4ecfc6e4b4ae10262044a2158bfac30ae
Kehinde Abioye <kabioye2@gmail.com>
2023-09-16 13:04:34 -0700

    fix betty style issue

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,14 +1,10 @@
 #include "3-calc.h"
 
 /**
- * This file contains the implementation of the operator functions:
- *
- * op_add - sum
- * op_sub
- * op_mul
- * op_div
- * op_mod
- *
+ * kdkmfjkjkngjkdgkjd
+ */
+
+/**
  * op_add - calculates sum of 2 integers
  * @a: first integer
  * @b: second integer
