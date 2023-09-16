f03d067dfe2aa5127b44bce5efdc9848d27e2f37
Kehinde Abioye <kabioye2@gmail.com>
2023-09-16 15:36:43 -0700

    added task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table -  prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	i = 0;
+	j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i * j;
+			if (product < 10 && j < 9)
+			{
+				_putchar('0' + product);
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			if (j < 9)
+			{
+				if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
