7dc5d81e14f12b3bb6332bd996b76d1b3e87b050
Kehinde Abioye <kabioye2@gmail.com>
2023-09-15 21:33:45 -0700

    fix betty issues

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,9 +5,8 @@
  * get_op_func - selects the correct function to perform the operation
  * asked by the user
  * @s: operator
- * Return: Return: Pointer to the function that corresponds to the operator
+ * Return: Pointer to the function that corresponds to the operator
  */
-
 
 int (*get_op_func(char *s))(int, int)
 {
