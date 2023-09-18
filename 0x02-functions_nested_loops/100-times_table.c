15cc84dc0dd08d42a49907afe57eae30fd7d0fd9
Kehinde Abioye <kabioye2@gmail.com>
2023-09-17 21:49:57 -0700

    added task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print times_table -  prints n times table, starting with 0
+ * @n: the integer
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int product;
+
+	i = 0;
+	j = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			product = i * j;
+
+			if (j > 0 && product < 10)
+			{
+				printf(" ");
+			}
+			printf("%d", product);
+
+			if (j < (n - 1))
+			{
+				printf(", ");;
+			}
+		}
+		printf('\n');
+	}
+}
