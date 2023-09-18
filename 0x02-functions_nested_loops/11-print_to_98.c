ac03f75a1312a833d1514e22ffe38dbcfc1729a9
Kehinde Abioye <kabioye2@gmail.com>
2023-09-17 21:26:03 -0700

    changed to printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,44 +13,28 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i < 10)
+			if (i != 98)
 			{
-				_putchar('0' + n);
+				printf("%d, ", i);
 			}
 			else
 			{
-				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
-			}
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d\n", i);
 			}
 		}
-		_putchar('\n');
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if (i < 10)
+			if (i != 98)
 			{
-				_putchar('0' + n);
+				printf("%d, ", i);
 			}
 			else
 			{
-				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
-			}
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d\n", i);
 			}
 		}
-		_putchar('\n');
 	}
 }
