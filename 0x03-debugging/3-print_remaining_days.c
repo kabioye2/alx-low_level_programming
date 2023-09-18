23e0539586c1ef282fc6b076cbb874df61ee424d
Kehinde Abioye <kabioye2@gmail.com>
2023-09-18 07:32:00 -0700

    fix output issue

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,7 +29,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", convert(month, day));
+			printf("Day of the year: %d\n", convert_day(month, day));
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
