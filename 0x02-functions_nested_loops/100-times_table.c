94dde532ac6c165cb198fa4128511f27d02b4a2b
Kehinde Abioye <kabioye2@gmail.com>
2023-09-17 23:46:50 -0700

    fix betty coding style issue

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,30 +5,31 @@
  * print_times_table -  prints n times table, starting with 0
  * @n: the integer
  */
-
 void print_times_table(int n)
 {
 	int i;
 	int j;
 	int product;
 	int spaces;
-
+	int temp;
 	i = 0;
 	j = 0;
-
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
-
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
-			spaces = (j > 0) ? ((product < 10) ? 3 : ((product < 100) ? 2 : 1)) : 0;
+			temp = product;
+			while (temp >= 10)
+			{
+				temp /= 10;
+				spaces++
+			}
 			printf("%*d", spaces, product);
-
 			if (j < n)
 			{
 				printf(",");
