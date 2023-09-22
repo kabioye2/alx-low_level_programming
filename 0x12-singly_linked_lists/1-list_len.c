b85f38013b91f4e9ddfb93cbef1e829e1f2cf826
Kehinde Abioye <kabioye2@gmail.com>
2023-09-21 21:00:08 -0700

    added task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/*
+ * list_len - returns the number of elements in a linked list
+ * @h: list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
