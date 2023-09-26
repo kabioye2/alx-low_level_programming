09ed693b6861accc9f2029e1aec3eaf8929f64f4
Kehinde Abioye <kabioye2@gmail.com>
2023-09-26 14:05:31 -0700

    added task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
