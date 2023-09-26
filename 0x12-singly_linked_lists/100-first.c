1ff0dfcc68f18306ddef324db864b37b69fa05a7
Kehinde Abioye <kabioye2@gmail.com>
2023-09-26 05:59:44 -0700

    fixed betty style issues

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * __atribute__ - constructor
+ * init - constructor
  */
 
 void __attribute__((constructor)) init(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
