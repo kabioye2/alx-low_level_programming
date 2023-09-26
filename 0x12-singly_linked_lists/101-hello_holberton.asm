8350718ec34c3d9ca4a99167c8687de8ceee1997
Kehinde Abioye <kabioye2@gmail.com>
2023-09-26 08:27:48 -0700

    added task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .rodata
+	format db "Hello, Holberton", 10, 0
+
+section .text
+	global main
+
+	extern printf
+
+main:
+	sub rsp, 8
+        lea rdi, [format]
+        call printf
+	add rsp, 8
+	mov rax, 60
+	xor rdi, rdi
+	syscall
---
+++ 0x12-singly_linked_lists/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int num = 42;
+    printf("Hello, Holberton! My favorite number is %d\n", num);
+    return 0;
+}
+
